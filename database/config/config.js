0bcda223b54ede656cebe50bfbffd7aa228a458a
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-05-13 12:56:42 -0300

    asdasdas

--- database/config/config.js
+++ database/config/config.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 module.exports={
   development: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
+    password: process.env.DB_PASSWORD || null,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'

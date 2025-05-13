2219fd6679c2eea41a84d6ee9b3b24ee4699860a
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-05-13 12:51:20 -0300

    deploy render

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,11 @@
+require('dotenv').config();
+
 module.exports={
-  "development": {
-    "username": "root",
-    "password": null,
-    "database": "fresquitas",
-    "host": "127.0.0.1",
-    "dialect": "mysql"
+  development: {
+    username: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    host: process.env.DB_HOST,
+    dialect: 'mysql'
   }
 }

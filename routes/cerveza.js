5995d92a9ee9a2be3e5da56f9428f9b63cdc07e6
ferminpachecoz <88350344+ferminpachecoz@users.noreply.github.com>
2025-05-19 11:43:07 -0300

    new route sin alcohol

--- routes/cerveza.js
+++ routes/cerveza.js
@@ -5,5 +5,6 @@ const cervezaController = require("../controllers/cervezaController")
 /* GET home page. */
 router.get('/baratas', cervezaController.listarBaratas);
 router.get('/buscar', cervezaController.buscarPorNombre);
+router.get('/sin-alcohol', cervezaController.listarSinAlcohol);
 
 module.exports = router;
